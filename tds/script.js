30623e0866df51911b0afafdf65467c419927123
Alan <thomasfraiz@gmail.com>
2026-05-07 01:30:01 -0300

    Update script.js

--- tds/script.js
+++ tds/script.js
@@ -2,32 +2,42 @@ const toggle = document.getElementById("menu-toggle");
 const menu = document.getElementById("menu");
 const overlay = document.getElementById("overlay");
 
-
 toggle.addEventListener("click", () => {
     menu.classList.toggle("active");
     overlay.classList.toggle("active");
 });
-
 
 overlay.addEventListener("click", () => {
     menu.classList.remove("active");
     overlay.classList.remove("active");
 });
 
-document.getElementById('whatsappForm').addEventListener('submit', function(e){
-            e.preventDefault();
+document
+    .getElementById("whatsappForm")
+    .addEventListener("submit", function(e) {
+        e.preventDefault();
 
-            const telefone = '5541999009999';
+        const telefone = "5541999009999";
 
-            const nome = document.getElementById('name').value;
-            const email = document.getElementById('email').value;
-            const mensagem = document.getElementById('mensagem').value;
+        const nome = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const mensagem = document.getElementById("mensagem").value;
 
-            const text = `Olá \nMeu nome é ${nome}, meu email é: ${email}.\nMensagem: ${mensagem}`;
+        const text = `Olá \nMeu nome é ${nome}, meu email é: ${email}.\nMensagem: ${mensagem}`;
 
-            const encodedText = encodeURIComponent(text);
+        const encodedText = encodeURIComponent(text);
 
-            const whatsappURL = `https://wa.me/${telefone}?text=${encodedText}`;
+        const whatsappURL = `https://wa.me/${telefone}?text=${encodedText}`;
 
-            window.open(whatsappURL, "_blank");
-        })
+        window.open(whatsappURL, "_blank");
+    });
+
+menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+    menu.classList.remove("active");
+    overlay.classList.remove("active");
+});
